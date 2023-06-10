c417664ac878a578fac9476013453d3cc12ecb2b
TRISTAN <debeaunetristan@gmail.com>
2023-06-10 17:05:31 +0200

    try fix CI

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,21 +4,22 @@
 auto main(int argc, char* argv[]) -> int
 {
     bool const is_CI = argc >= 2 && strcmp(argv[1], "-CI") == 0; // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+
     if (is_CI)
         return 0; // In CI, knowing that the executable can run and doesn't give a "missing dll" error at runtime is enough.
 
-    cv::VideoCapture cap{0};
-    if (!cap.isOpened())
-    {
-        std::cout << "Cannot access webcam.\n";
-        return -1;
-    }
+    // cv::VideoCapture cap{0};
+    // if (!cap.isOpened())
+    // {
+    //     std::cout << "Cannot access webcam.\n";
+    //     return -1;
+    // }
 
-    cv::Mat image;
-    while (true)
-    {
-        cap >> image;
-        cv::imshow("Hello OpenCV", image);
-        cv::waitKey(25);
-    }
+    // cv::Mat image;
+    // while (true)
+    // {
+    //     cap >> image;
+    //     cv::imshow("Hello OpenCV", image);
+    //     cv::waitKey(25);
+    // }
 }
