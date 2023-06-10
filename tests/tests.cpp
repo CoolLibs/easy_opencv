1369ce5ddf0bb98a115d1384d69c523129e24be8
TRISTAN <debeaunetristan@gmail.com>
2023-06-10 17:11:06 +0200

    test CI 2

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,8 +5,10 @@ auto main(int argc, char* argv[]) -> int
 {
     bool const is_CI = argc >= 2 && strcmp(argv[1], "-CI") == 0; // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
 
-    if (is_CI)
-        return 0; // In CI, knowing that the executable can run and doesn't give a "missing dll" error at runtime is enough.
+    cv::Mat image;
+    (void)image;
+    // if (is_CI)
+    //     return 0; // In CI, knowing that the executable can run and doesn't give a "missing dll" error at runtime is enough.
 
     // cv::VideoCapture cap{0};
     // if (!cap.isOpened())
@@ -16,10 +18,9 @@ auto main(int argc, char* argv[]) -> int
     // }
 
     // cv::Mat image;
-    // while (true)
+    // while (cv::waitKey(25) != 13) // 13 => ENTER
     // {
     //     cap >> image;
     //     cv::imshow("Hello OpenCV", image);
-    //     cv::waitKey(25);
     // }
 }
