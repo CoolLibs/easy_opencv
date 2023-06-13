7a70536f1de36ec4874fca0b74e6402fdf47065f
TRISTAN <debeaunetristan@gmail.com>
2023-06-13 10:01:42 +0200

    🐛 try to get .exe from CI

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,22 +5,22 @@ auto main(int argc, char* argv[]) -> int
 {
     bool const is_CI = argc >= 2 && strcmp(argv[1], "-CI") == 0; // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
 
-    cv::Mat image;
-    (void)image;
-    // if (is_CI)
-    //     return 0; // In CI, knowing that the executable can run and doesn't give a "missing dll" error at runtime is enough.
-
-    // cv::VideoCapture cap{0};
-    // if (!cap.isOpened())
-    // {
-    //     std::cout << "Cannot access webcam.\n";
-    //     return -1;
-    // }
-
     // cv::Mat image;
-    // while (cv::waitKey(25) != 13) // 13 => ENTER
-    // {
-    //     cap >> image;
-    //     cv::imshow("Hello OpenCV", image);
-    // }
+    // (void)image;
+    if (is_CI)
+        return 0; // In CI, knowing that the executable can run and doesn't give a "missing dll" error at runtime is enough.
+
+    cv::VideoCapture cap{0};
+    if (!cap.isOpened())
+    {
+        std::cout << "Cannot access webcam.\n";
+        return -1;
+    }
+
+    cv::Mat image;
+    while (cv::waitKey(25) != 27) // 27 => ESCAPE
+    {
+        cap >> image;
+        cv::imshow("Hello OpenCV", image);
+    }
 }
